6f372e23182720fc977bfd00c4ed7302f57d83d7
Aetrix27 <davedogg323@gmail.com>
2020-09-08 18:38:07 -0700

    Finished Program
    Added functions that make calculations such as the area and return results.

---
+++ functions.py
@@ -0,0 +1,35 @@
+def area(width, height):
+    result = width * height
+    return result
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+print("Area Results: ")
+result = area(5, 6)
+print(result)
+result2= area(10.5,17.6)
+print(result2)
+result3 = area(3.78,2)
+print(result3)
+
+print("Subtract Results:")
+subtract_result1 = subtract(3,1)
+print(subtract_result1)
+subtract_result2 = subtract(3,5)
+print(subtract_result2)
+
+print("Add Results:")
+add_result1=add(3,89)
+print(add_result1)
+add_result2=add(8.7,90.43)
+print(add_result2)
